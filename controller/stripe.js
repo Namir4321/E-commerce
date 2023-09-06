c2f326ad468467a0243430213f8700be763ebf8c
Namir4321 <97880446+Namir4321@users.noreply.github.com>
2023-09-06 13:17:52 +0530

    Update stripe.js

--- controller/stripe.js
+++ controller/stripe.js
@@ -1,6 +1,8 @@
 const stripe = require("stripe")(process.env.SECRET_KEY_PAY);
 exports.stripePayment = async (req, res, next) => {
   const token=req.body;
+   const baseUrls = process.env.BaseUrl.split(',');
+  const baseUrl = baseUrls[0].trim();
   try{
   const lineItems = token.map((product)=>({
     price_data:{
@@ -23,10 +25,9 @@ exports.stripePayment = async (req, res, next) => {
             allowed_countries: [ "AC", "AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AQ", "AR", "AT", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BV", "BW", "BY", "BZ", "CA", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CV", "CW", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK", "FO", "FR", "GA", "GB", "GD", "GE", "GF", "GG", "GH", "GI", "GL", "GM", "GN", "GP", "GQ", "GR", "GS", "GT", "GU", "GW", "GY", "HK", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IM", "IN", "IO", "IQ", "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM", "KN", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD", "ME", "MF", "MG", "MK", "ML", "MM", "MN", "MO", "MQ", "MR", "MS", "MT", "MU", "MV", "MW",
         "MX", "MY", "MZ", "NA", "NC", "NE", "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PN", "PR", "PS", "PT", "PY", "QA", "RE", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SE", "SG", "SH", "SI", "SJ", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SX", "SZ", "TA", "TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "US", "UY", "UZ", "VA", "VC", "VE", "VG", "VN", "VU", "WF", "WS", "XK", "YE", "YT", "ZA", "ZM", "ZW", "ZZ"],
           },
-          
-        success_url:`${process.env.BaseUrl}/success?sessionId={CHECKOUT_SESSION_ID}`,
-    cancel_url:`${process.env.BaseUrl}/cart`,
-    });
+            success_url: `${baseUrl}/success?sessionId={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${baseUrl}/cart`,
+       });
    
 
 res.status(200).json({id:session.id,session})
